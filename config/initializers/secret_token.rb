896f1f6b9dd97456a34b3e3aa6645f366d094fb4
RockingChair LLC <rockingchairllc@gmail.com>
2012-03-15 19:21:00 -0400

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Invest::Application.config.secret_token = '9233f8f16d9c6d47b2f789c80d676d36981bd93f8529b74fb948e74048405318cf97b89166d155f847ac25d33af2894b9c800f2731b728713e803bc49d006b3b'
